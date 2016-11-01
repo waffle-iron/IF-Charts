7867ecd4779663dd43203c3d0e85400333524775
Giacomo Lawrance <giacomolawrance@gmail.com>
2016-11-01 18:05:36 +0000

    added more info to rotation.js
    Added info that people should try landscape before reporting problems.

--- IF-Charts/js/rotation.js
+++ IF-Charts/js/rotation.js
@@ -1,3 +1,3 @@
 if(window.innerHeight > window.innerWidth){
-    alert("This page is best viewed landscape");
+    alert("This page is best viewed landscape. Having problems with the site? Please try the landscape orientation (chances are that it will fix it).");
 }
